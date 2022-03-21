1cd854d4e753777c4ae6bd6b98f39038441a7ef4
Matthew Brazill <Matthew@Brazill.de>
2022-03-21 04:37:54 +0000

    Secuity solution wasnt somuch solution after all... reverting and hoping for best is my only real option right now i guess...

--- app.js
+++ app.js
@@ -34,11 +34,7 @@ ssm.getParameters({ Names: ['witkc-session-key'], WithDecryption: true }, (err, 
                     region: 'eu-west-1'
                 },
             }),
-            cookie: {
-                secure: true,
-                httpOnly: false,
-                maxAge: 1000 * 60 * 60 * 12
-            },
+            cookie: { maxAge: 1000 * 60 * 60 * 12 },
             resave: false
         }))
         logger.info(`Session Key Loaded!`)
